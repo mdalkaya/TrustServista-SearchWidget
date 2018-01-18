2b953e2eca0e26fda6f632e15c83d90dc63de275
mdalkaya <34447892+mdalkaya@users.noreply.github.com>
2018-01-18 14:31:17 +0100

    Update fetch.js

--- src/fetch.js
+++ src/fetch.js
@@ -1,145 +1,434 @@
-import TimeAgo from "javascript-time-ago";
-// Load locale-specific relative date/time formatting rules.
-import en from "javascript-time-ago/locale/en";
-export function execute_fetch(query, page) {
-	var tokenpromise = new Promise(function(resolve, reject) {
-		var user = "annova";
-		var password = "Medox124";
-		var loginURL = "https://demo.medox.scisys.de:8443/dira6/auth/v10/login";
-		var tok = user + ":" + password;
-		var hash = btoa(tok);
-		var today = new Date();
+import React from "react";
+import TimeAgo from "react-timeago";
+import { execute_fetch } from "./fetch.js";
+import ScrollableAnchor from "react-scrollable-anchor";
+import { CardExpandable } from "./CardExpandable.js";
+import { render } from "react-dom";
+import _ from "lodash";
+import { AppContainer } from 'react-hot-loader'
 
-		if (
-			localStorage.getItem("medox_access_token") === null ||
-			localStorage.getItem("medox_access_token_expiry_date") < today.getTime()
-		) {
-			fetch(loginURL, {
-				method: "GET",
-				headers: {
-					Authorization: "Basic " + hash,
-					Accept: "application/json"
-				}
-			})
-				.then(response => response.json())
-				.then(data => {
-					localStorage.setItem("medox_access_token", data.access_token);
-					var expiryDateTimeMS = today.getTime() + data.expires_in * 1000;
-					localStorage.setItem(
-						"medox_access_token_expiry_date",
-						expiryDateTimeMS
-					);
-					console.log(data.access_token);
-					resolve(data.access_token);
-				})
-				.catch(error => console.warn(error));
-		} else {
-			resolve(localStorage.getItem("medox_access_token"));
-		}
-	});
 
-	//one we have a valid token, we actually execute the fetch
-	return tokenpromise.then(function(token) {
-        //console.log(token);
-      
-		var searchURL =
-			"https://demo.medox.scisys.de:8443/dira6/api/v10/search/contentItems";
+import {
+	Grid,
+	Rail,
+	Image,
+	Accordion,
+	Tab,
+	Menu,
+	Icon,
+	Card,
+	Button,
+	Divider,
+	Label,
+	Header,
+	Input,
+	Container,
+	Dropdown,
+	Popup,
+	Segment,
+	Search,
+	List,
+	Form,
+	Modal,
+	Item,
+	Dimmer,
+	Message,
+	Loader,
+	TextArea,
+	Checkbox,
+	Sidebar,
+	Breadcrumb,
+	Feed,
+	Comment,
+	Responsive
+} from "semantic-ui-react";
 
-		return fetch(searchURL, {
-			method: "POST",
-			headers: {
-				Authorization: "Bearer " + token,
-				Accept: "application/json",
-				"Content-Type": "application/json"
+class WidgetWithCards extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleFilterChange = this.handleFilterChange.bind(this);
+		this.handleRefresh = this.handleRefresh.bind(this);
+		this.handleApplyFilters = this.handleApplyFilters.bind(this);
+		this.resetFilters = this.resetFilters.bind(this);
+		this.handleKeepOpened = this.handleKeepOpened.bind(this);
+		this.handleSearchSideBar = this.handleSearchSideBar.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+
+		this.state = {
+			page_to_fetch: 0,
+			loadingresults: false,
+			searchSideBarVisible: false,
+			keepOpened: false,
+			filtersHaveChanged: false,
+			filterButtonColor: null,
+			keepOpened: false,
+			query: "",
+			resultCount: "No search started",
+			articles: {
+				status: "ok",
+				source: "abc-news-au",
+				sortBy: "top",
+				articles: ""
 			},
-			body: JSON.stringify({
-				query: {
-					bool: {
-						must: [{ index: { meta: query + " " } }],
-						filter: [{ term: { status: "valid" } }]
-					}
+			allCards: <div />,
+			newCards: <div />
+		};
+	}
+
+	componentDidMount() {
+		this.handleRefresh();
+	}
+	handleRefresh(e) {
+		this.setState({ page_to_fetch: ++this.state.page_to_fetch });
+
+		// console.log(this.state.allCards);
+
+		this.setState({
+			loadingresults: true,
+			resultCount: "Searching..."
+		});
+
+		this.setState(prevState => ({
+			allCards: (
+				<div>
+					{" "}
+					<Loader active />
+				</div>
+			)
+		}));
+
+		execute_fetch(this.state.query, this.state.page_to_fetch).then(
+			function(data) {
+			
+				this.setState({
+					resultCount: data.resultCount + " result(s)"
+				});
+
+				var previousCard = this.state.allCards;
+
+				var cardsHTML = (
+					<Card.Group unstackable={true} divided={true}>
+						{this.state.loadingresults ? (
+							data.items.map(item => (
+								<CardExpandable
+									fluid
+									key={item._id}
+									formattedItem={item}
+									rawItem={item.rawItem}
+									iconName={item.iconName}
+									iconColor={item.iconColor}
+									mediaType={item.mediaType}
+									thumbnail={item.thumbnail}
+									highres={item.highres}
+									description={item.description}
+									title={item.title}
+									open_url={item.open_url}
+									dragAndDropString={item.dragAndDropString}
+									target={item.target}
+									meta={item.meta}
+								/>
+							))
+						) : (
+							<Loader active />
+						)}
+					</Card.Group>
+				);
+
+				this.setState(prevState => ({
+					allCards: <div>{cardsHTML}</div>
+				}));
+			}.bind(this)
+		);
+	}
+	handleKeepOpened(e) {
+		this.setState({
+			keepOpened: !this.state.keepOpened
+		});
+	}
+	handleSearchSideBar(e) {
+		this.setState({
+			searchSideBarVisible: !this.state.searchSideBarVisible
+		});
+	}
+
+	handleFilterChange(e) {
+		this.setState({
+			filtersHaveChanged: true
+		});
+	}
+
+	handleApplyFilters(e) {
+		if (this.state.filtersHaveChanged) {
+			this.setState({
+				filterButtonColor: "orange"
+			});
+		}
+
+		if (!this.state.keepOpened) {
+			this.setState({
+				searchSideBarVisible: false
+			});
+		}
+	}
+
+	resetFilters(e) {
+		this.setState({
+			filtersHaveChanged: false,
+			filterButtonColor: null
+		});
+	}
+	handleKeyPress(e) {
+		if (e.charCode == 13) {
+			this.handleRefresh();
+			console.log(this.state.query);
+		}
+	}
+
+	render() {
+		let panels = [
+			{
+				active: true,
+				title: {
+					key: "sort",
+					children: (
+						<Header as="h4" dividing>
+							<Header.Content>
+								<Icon name="list" />View
+							</Header.Content>
+						</Header>
+					)
 				},
-				from: 1,
-				size: 20,
-				sort: [{ modTime: { order: "desc" } }]
-			})
-		})
-			.then(response => response.json())
-			.then(responseData => {
-		//		console.log(responseData);
-
-				TimeAgo.locale(en);
-				const timeAgo = new TimeAgo("en-US");
-
-				var len = responseData.contentItems.length,
-					//newData = { resultCount: responseData.count, items: [] },
-					newData = { resultCount: responseData.count, items: [] },
-					i;
-		//		console.log(len);
-
-				//Loop through the source JSON and format it into the standard format
-				for (i = 0; i < len; i += 1) {
-					newData.items.push({
-						id: responseData.contentItems[i]._id,
-						key: responseData.contentItems[i]._id,
-						rawItem: responseData.contentItems[i],
-						title: responseData.contentItems[i].mainTitle,
-						open_url: "https://demo.medox.scisys.de:8443/",
-						description:
-							"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-
-						target: "_blank",
-						iconName: responseData.contentItems[i].hasOwnProperty("images")
-							? "image"
-							: responseData.contentItems[i].hasOwnProperty("videos")
-								? "film"
-								: "volume up",
-						iconColor: responseData.contentItems[i].hasOwnProperty("images")
-							? "green"
-							: responseData.contentItems[i].hasOwnProperty("videos")
-								? "blue"
-								: "red",
-						mediaType: responseData.contentItems[i].hasOwnProperty("images")
-							? "image"
-							: responseData.contentItems[i].hasOwnProperty("videos")
-								? "video"
-								: "audio",
-
-						thumbnail: responseData.contentItems[i].hasOwnProperty("images")
-							? responseData.contentItems[i].images[0].variants[0].url
-							: responseData.contentItems[i].hasOwnProperty("videos")
-								? "https://react.semantic-ui.com/assets/images/image-16by9.png"
-								: null,
-						highres: responseData.contentItems[i].hasOwnProperty("images")
-							? responseData.contentItems[i].images[0].variants[0].url
-							: responseData.contentItems[i].hasOwnProperty("videos")
-								? responseData.contentItems[i].videos[0].variants[0].url
-								: responseData.contentItems[i].audios[0].variants[1].url,
-
-						dragAndDropString:
-							"<mos><mosID>DIRA.DEMO.AUDIO.MOS</mosID>" +
-							"<objID>" +
-							responseData.contentItems[i]._id +
-							"</objID>" +
-							"<objSlug>" +
-							responseData.contentItems[i].mainTitle +
-							"</objSlug>" +
-							"<objTB>48000</objTB> <objDur>497664</objDur>" +
-							"<mosAbstract>" +
-							responseData.contentItems[i].mainType.displayName +
-							"</mosAbstract></mos>",
-
-						meta:
-							responseData.contentItems[i].mainType.displayName +
-							"&nbsp;&middot;&nbsp" +
-							responseData.contentItems[i].creaUser +
-							"&nbsp;&middot;&nbsp" +
-							timeAgo.format(new Date(responseData.contentItems[i].creaTime))
-					});
+				content: {
+					key: "sortContent",
+					children: (
+						<div>
+							Fields
+							<List selection>
+								{" "}
+								<List.Item>
+									<List.Icon name="" />
+									<List.Content>Description</List.Content>
+								</List.Item>
+								<List.Item active>
+									<List.Icon name="check" />
+									<List.Content>Metadata</List.Content>
+								</List.Item>
+							</List>
+							Layout
+							<List selection>
+								{" "}
+								<List.Item>
+									<List.Icon name="" />
+									<List.Content>Grid</List.Content>
+								</List.Item>
+								<List.Item active>
+									<List.Icon name="check" />
+									<List.Content>List</List.Content>
+								</List.Item>
+							</List>
+							Image
+							<List selection>
+								{" "}
+								<List.Item>
+									<List.Icon name="" />
+									<List.Content>None</List.Content>
+								</List.Item>
+								<List.Item active>
+									<List.Icon name="check" />
+									<List.Content>Small</List.Content>
+								</List.Item>
+								<List.Item>
+									<List.Icon name="" />
+									<List.Content>Large</List.Content>
+								</List.Item>
+								<List.Item>
+									<List.Icon name="" />
+									<List.Content>Very large</List.Content>
+								</List.Item>
+							</List>
+						</div>
+					)
 				}
-				//console.log(JSON.stringify(newData));
-				return newData;
-			})
-			.catch(error => console.warn(error));
-	});
+			},
+			{
+				active: true,
+				title: {
+					key: "data",
+					children: (
+						<Header as="h4" dividing>
+							<Header.Content>
+								<Icon name="sort content descending" />Sort
+							</Header.Content>
+						</Header>
+					)
+				},
+				content: {
+					key: "dataContent",
+					children: (
+						<List selection>
+							{" "}
+							<List.Item active>
+								<List.Icon name="arrow down" />
+								<List.Content>Creation Date</List.Content>
+							</List.Item>
+							<List.Item>
+								<List.Icon name="" />
+								<List.Content>Update time</List.Content>
+							</List.Item>
+						</List>
+					)
+				}
+			},
+			{
+				title: {
+					key: "filters",
+					children: (
+						<Header as="h4" dividing>
+							<Header.Content>
+								<Icon name="filter" />Filters
+							</Header.Content>
+						</Header>
+					)
+				},
+				content: {
+					key: "filtersContent",
+					children: (
+						<Form>
+							<Form.Field>
+								<label>placeholder</label>
+								<Input />
+							</Form.Field>
+						</Form>
+					)
+				}
+			}
+		];
+		return (
+			<div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+				<div style={{ margin: "5px", flex: "0" }}>
+					<Input
+						onChange={(e, data) => {
+							this.setState({ query: data.value });
+						}}
+						onKeyPress={this.handleKeyPress}
+						action={
+							<Button
+							basic
+								icon="options"
+								onClick={this.handleSearchSideBar}
+							/>
+						}
+						placeholder="Search"
+						defaultValue=""
+						fluid
+					/>
+				</div>
+
+				<Divider horizontal fitted>
+					<Label size="small" color="grey">
+						{this.state.resultCount}
+					</Label>
+				</Divider>
+
+				<Sidebar.Pushable style={{ height: "100%" }}>
+					<Sidebar
+						animation="overlay"
+						direction="right"
+						style={{
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "space-between",
+							overflowX: "hidden",
+							maxHeight: "100%"
+						}}
+						visible={this.state.searchSideBarVisible}>
+						<Segment
+							attached
+							className="fancy-scrollbar"
+							style={{
+								overflowX: "hidden",
+								overflowY: "auto",
+								height: "100%",
+								flex: "1"
+							}}>
+							<Accordion panels={panels} defaultActiveIndex={0} />
+						</Segment>
+
+						<Segment attached style={{ flex: "0" }}>
+							<Checkbox label="Keep opened" onClick={this.handleKeepOpened} />
+							<p />
+							<Button
+								primary
+								content="Apply"
+								floated="left"
+								onClick={this.handleApplyFilters}
+							/>
+							<Button.Group basic floated="right">
+								<Button icon="eraser" onClick={this.resetFilters} />
+								<Button icon="save" />
+								<Button icon="bell" />
+							</Button.Group>
+						</Segment>
+					</Sidebar>
+					<Sidebar.Pusher
+						className="fancy-scrollbar card-list"
+						style={{
+							overflowY: "auto",
+							display: "flex",
+							flex: "1",
+							flexDirection: "column",
+							height: "100%"
+						}}>
+						{this.state.allCards}
+
+						{/*
+						<Divider horizontal>
+							<Button content="Load more" onClick={this.handleRefresh} />
+						</Divider>
+						*/}
+					</Sidebar.Pusher>
+				</Sidebar.Pushable>
+			</div>
+		);
+	}
 }
+class App extends React.Component {
+	render() {
+		return (
+			<div
+				className="fullscreen"
+				style={{
+					display: "flex",
+					flexDirection: "column",
+					height: "100vh",
+					justifyContent: "space-between"
+				}}>
+				<WidgetWithCards style={{ height: "100%" }} />
+			</div>
+		);
+	}
+}
+
+
+
+// ----------------------------------------
+// Render
+// ----------------------------------------
+//export default App;
+
+render(<AppContainer><App /></AppContainer>, document.getElementById('root'));
+
+
+ if (process.env.NODE_ENV !== 'production') {
+     console.log('Looks like we are in development mode!');
+   }
+
+
+var currentFile = require.resolve('./index.js');
+if (module.hot) {
+  module.hot.accept(currentFile, () => { 
+  		const App = require(currentFile).default;
+		render(<AppContainer> <App /></AppContainer>, document.getElementById('root'));
+  })
+}
+
